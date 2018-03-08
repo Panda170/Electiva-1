ae1bda46f9d763d55bbac149511e5e14b4483bfe
Daniel Felipe Contreras López <cldfrkr@hotmail.com>
2018-03-07 19:59:53 -0500

    test jqrry

--- DemoJQuery/js/demoJQueryMio.js
+++ DemoJQuery/js/demoJQueryMio.js
@@ -1,6 +1,9 @@
 // JavaScript Document
-
-
+//$('img[src="./Universidad Pedagógica y Tecnológica de Colombia_files/banner_movi_4147.jpg"]').attr('src', '../media/ima/hndsz.jpg');
+$('img[src="./Universidad Pedagógica y Tecnológica de Colombia_files/banner_movi_4147.jpg"]').attr('id', 'a123');
+$('img[id="a123"]').attr('src', '../media/ima/hndsz.jpg');
+$('img[id="a123"]').attr('width', '930');
+$('img[id="a123"]').attr('height', '330');
 $("body").css('background-color', 'black');
 $("body").css('font-family', 'Lucida Calligraphy');
 $("body").css('color', 'white');
@@ -14,7 +17,7 @@ $("a").css('background-color', 'black');
 for (var i = 0; i < $("h5").length; i++) { 
 	if($($("h5")[i]).text() == "Estudiantes de Biología seleccionados por el Real Jardín Botánico de Kew "){
 		console.log($($("h5")[i]).text());
-		$($("h5")[i]).html("Cambio de texto por una nueva noticia");
+		$($("h5")[i]).html("Cambio de texto por una nueva noticia")
 	}
     
 }
